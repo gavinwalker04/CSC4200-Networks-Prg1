cb7b0861fda9ad5a091439940ff9a3f41ee696c6
Gavin Walker <gavinwalker574@gmail.com>
2025-03-16 15:59:25 -0500

    added encrypt/decrypt funcs

--- client.py
+++ client.py
@@ -11,9 +11,27 @@ HOST = socket.gethostbyname(socket.gethostname())
 FORMAT = 'utf-8'
 ADDR = (HOST, PORT)
 DISCONNECT_MESSAGE = "!DISCONNECT"
+KEY = b'This is a key123This is a key123'  # 32-byte AES key (Must be 16, 24, or 32 bytes)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+
+# AES Encryption Function
+def encrypt(plaintext, key):
+    """Encrypts a message using AES in CBC mode."""
+    iv = os.urandom(16)  # Generate a random 16-byte IV
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    ciphertext = cipher.encrypt(pad(plaintext.encode(), AES.block_size))
+    return iv + ciphertext  # Prepend IV to ciphertext for decryption
+
+
+# AES Decryption Function
+def decrypt(ciphertext, key):
+    """Decrypts a message using AES in CBC mode."""
+    iv = ciphertext[:16]  # Extract IV from the message
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    plaintext = unpad(cipher.decrypt(ciphertext[16:]), AES.block_size)
+    return plaintext.decode()
 
 def send(msg):
     message = msg.encode(FORMAT)
